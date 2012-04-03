d3265f3a5766692d63909ff7f723964772f4c7a3
Travis Hotz <travishotz@gmail.com>
2012-04-03 15:30:44 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DollarBet::Application.config.secret_token = '8cf8d4f08ffd1f5d798f40a7f5cad4ee5e7fc0bf57ee773d79bf6c896e5d14b5fae720ef6ca93dd3c18720fcbfa5ebadc179bb32891dd540cc040f767b14a849'
